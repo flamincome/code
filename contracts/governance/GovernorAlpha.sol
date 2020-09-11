7f550d26dc0ebadf7116f6b0a7011641e49b7eda
Albert <github@albert.sh>
2020-09-12 01:54:01 +0200

    Make supply dynamic and adjust rates

--- contracts/governance/GovernorAlpha.sol
+++ contracts/governance/GovernorAlpha.sol
@@ -6,10 +6,10 @@ contract GovernorAlpha {
     string public constant name = "Compound Governor Alpha";
 
     /// @notice The number of votes in support of a proposal required in order for a quorum to be reached and for a vote to succeed
-    function quorumVotes() public pure returns (uint) { return 400000e18; } // 400,000 = 4% of Comp
+    function quorumVotes() public pure returns (uint) { return (comp.totalSupply * 3) / 10; } // 30% of total FLAM Supply
 
     /// @notice The number of votes required in order for a voter to become a proposer
-    function proposalThreshold() public pure returns (uint) { return 100000e18; } // 100,000 = 1% of Comp
+    function proposalThreshold() public pure returns (uint) { return comp.totalSupply / 100; } // 1% of total FLAM Supply
 
     /// @notice The maximum number of actions that can be included in a proposal
     function proposalMaxOperations() public pure returns (uint) { return 10; } // 10 actions
@@ -328,4 +328,5 @@ interface TimelockInterface {
 
 interface CompInterface {
     function getPriorVotes(address account, uint blockNumber) external view returns (uint96);
+    function totalSupply() public view returns (uint);
 }
